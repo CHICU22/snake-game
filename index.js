e9923b771ecd86bd1b1106f65687c3de56953be5
Beezyfx33 <84154667+Beezyfx33@users.noreply.github.com>
2022-03-19 23:36:11 +0800

    adicionado corpo cobra

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var rows = 10;
-var columns = 10;
+var rows = 30;
+var columns = 30;
 
 gridContainer = document.getElementById('gridContainer');
 for (let rowNumber = 0; rowNumber<rows; rowNumber++){
@@ -12,21 +12,25 @@ for (let rowNumber = 0; rowNumber<rows; rowNumber++){
         var gridItem = document.createElement('div');
         row.appendChild(gridItem);
         gridItem.classList.add('gridItem');
-        gridItem.id = $(rowNumber)-$(colNumber);
+        gridItem.id = (rowNumber) - (colNumber);
     }
 }
 
-snakePosition = [
+
+
+/*snakePosition = [
     [2, 2],
     [2, 3],
     [2, 4],
 ];
 
-function render(){
-    snakePositions.forEach(snakePosition) => {
-        gridItem = documentElementById(
-            ${snakePosition[0]}
-        )
-    }
+function drawSnake(gridContainer){
+    snakePositions.forEach(snakePosition => {
+        const snake = document.createElement('div')
+        snake.style.gridRowStart = snakePosition.x
+        snake.style.gridColumnStart = snakePosition.y
+        snake.classList.add('snake');
+        gridContainer.appendChild(snake)
+    })
+}  */
 
-}
